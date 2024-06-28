7ba596efd87d58cb303c47e95dac299b98fb3fa0
queen-favour <elechifavour3@gmail.com>
2024-06-28 01:17:53 +0100

    working on individual categories men,women, kids

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <section className="max_padd_container flexCenter flex-col pt-32">
+      <div>
+        <h3>Sign Up</h3>
+        <div>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p>
+          Already have an account? <span>Login</span>
+        </p>
+        <div>
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
